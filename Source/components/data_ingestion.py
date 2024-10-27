2bff148494f949d334b3446013aaa5066fdb2b7b
charan-charlie <derangulacharandcharan@gmail.com>
2024-10-27 23:34:31 +0530

    artifacts and data_ingestion code added

--- Source/components/data_ingestion.py
+++ Source/components/data_ingestion.py
@@ -1,2 +1,58 @@
-## reading the data from data base is called data ingestion.
-## we will divide the data train and test
+## reading the data from the database is called data ingestion.
+## we will divide the data into train and test sets
+import os
+import sys
+from Source.exception import CustomException
+from Source.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join("artifacts", "train.csv")  # Data ingestion output will be saved here
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "raw.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        # If the data is stored in databases, we use this function to read it from the database
+        logging.info("Entered the data ingestion method or component")
+        try:
+            # Ensure you have the correct path to your dataset file
+            df = pd.read_csv(r"C:\Users\deran\Desktop\ML_Project_1\notebook\Data\stud.csv")
+
+            logging.info("Read the dataset as dataframe")
+
+            # Creating directories for storing train, test, and raw data files
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            # Save the raw data
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            
+            # Split data into train and test sets
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            # Save the train and test sets to their respective paths
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            # Return paths for train and test sets
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
